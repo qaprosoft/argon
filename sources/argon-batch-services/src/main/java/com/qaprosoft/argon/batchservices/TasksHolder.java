0c87b14b6c2f5e1ecb7661a2d1ba1e4b529547dd
Alex Semenkov <asemenkov@onflydev.com>
2017-12-27 10:32:08 +0300

    Email confirmation batch job

---
+++ sources/argon-batch-services/src/main/java/com/qaprosoft/argon/batchservices/TasksHolder.java
@@ -0,0 +1,27 @@
+package com.qaprosoft.argon.batchservices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import com.qaprosoft.argon.services.services.impl.ConfirmationService;
+import com.qaprosoft.argon.services.services.impl.UserService;
+
+@Configuration
+@ImportResource("classpath:argon-services.xml")
+public class TasksHolder
+{
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private ConfirmationService confirmationService;
+
+	@Bean
+	public Runnable sendConfirmationEmailTask()
+	{
+		return () -> userService.getUsersForConfirmationMailing().stream()
+				.forEach(confirmationService::generateUserConfirmation);
+	}
+}
--- sources/argon-batch-services/src/main/java/com/qaprosoft/argon/batchservices/service/ISchedulerService.java
+++
@@ -1,6 +0,0 @@
-package com.qaprosoft.argon.batchservices.service;
-
-public interface ISchedulerService
-{
-	public void executeAbortFrozenTestRunsTask();
-}
